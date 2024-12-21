108e7f03855389e6ad481846186a52d8092aae97
Oluwatunmise-olat <oluwatunmiseolatunbosun2001@gmail.com>
2024-12-21 02:02:52 +0100

    chore: add basic express endpoints

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const http = require("http")
+
+const server = express()
+
+const PORT = process.env.PORT || 3000;
+
+server.use(express.json());
+
+server.get("/health", (req, res) => {
+  res.status(200).send({ status: "Healthy", timestamp: new Date() });
+});
+
+// Endpoint to display secret (for testing updates)
+server.get("/app-name", (req, res) => {
+  const appName = process.env.APP_NAME || null;
+
+  res.status(200).send({ application_name: appName });
+});
+
+
+http.createServer(server).listen(PORT, () => console.log(`Demo application starting on port ${PORT} 🧨`))
